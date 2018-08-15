0010e9f57a5c4602c8336c9771f19793a1a6a40a
Jan Schulte <jan@janschulte.com>
2018-08-15 19:42:09 +0200

    Cleanup scripts.

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,17 +1,10 @@
 #!/bin/sh
 
-#./server.sh & ./hotreload.sh & wait
-#sh server.sh &
-#reload() {
-#  stack build --fast --file-watch-poll --exec 'echo "YEAH"; killall servant-example-exe'
-#}
-#
 server() {
   while true; do echo 'Starting server'; stack exec servant-example-exe; echo 'Server stopped'; sleep 1; done
 }
 echo 'Starting server loop...'
-server & # Start server loop
+server & 
+
 echo 'Starting file watcher...'
 stack build --fast --file-watch-poll --exec 'killall servant-example-exe'
-
-#server & reload & wait
