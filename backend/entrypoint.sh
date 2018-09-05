59f141a02b237ec6f692cf5eaa5c982e1434fba4
Jan Schulte <jan@janschulte.com>
2018-09-05 20:58:35 +0200

    Fix build

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+# initial build
+stack build
+
 server() {
-  while true; do echo 'Starting server'; stack exec servant-example-exe; echo 'Server stopped'; sleep 1; done
+  while true; do echo 'Starting server'; stack exec spa-template-exe; echo 'Server stopped'; sleep 1; done
 }
 echo 'Starting server loop...'
 server & 
 
 echo 'Starting file watcher...'
-stack build --fast --file-watch-poll --exec 'killall servant-example-exe'
+stack build --fast --file-watch-poll --exec 'killall spa-template-exe'
--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+# initial installation of packages and initial build
+psc-package install && pulp build 
+
+# hot reload  
 pulp --watch build & parcel index.html --hmr-port 1235 & wait
