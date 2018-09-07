b63d4fc7dbfda6912172466323f2f71f866475e2
janschultecom <jan@janschulte.com>
2018-09-07 08:38:50 +0200

    Improve backend entrypoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 # initial build
-stack build
+echo 'Starting initial haskell build... this may take a veeeery long time! Grab a coffee ☕'
+stack clean build
 
 server() {
   while true; do echo 'Starting server'; stack exec spa-template-exe; echo 'Server stopped'; sleep 1; done
 }
 echo 'Starting server loop...'
-server & 
+server &
 
 echo 'Starting file watcher...'
 stack build --fast --file-watch-poll --exec 'killall spa-template-exe'
